ec4a49d167fa8323527530c8fcadd02cfb717d1c
Apoorva Bapat <48076182+apoorva-bapat@users.noreply.github.com>
2019-05-02 21:51:00 -0500

    BuildDataSet

---
+++ builddataset.py
@@ -0,0 +1,12 @@
+#Building dataset
+
+def buildTestSet(search_keyword):
+    try:
+        tweets_fetched = twitter_api.GetSearch(search_keyword, count = 100)
+        
+        print("Fetched " + str(len(tweets_fetched)) + " tweets for the term " + search_keyword)
+        
+        return [{"text":status.text, "label":None} for status in tweets_fetched]
+    except:
+        print("Unfortunately, something went wrong..")
+        return None
