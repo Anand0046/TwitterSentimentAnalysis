e1c51b7e9fba6ecd39b1c634010a94a8b69ceca4
Apoorva Bapat <48076182+apoorva-bapat@users.noreply.github.com>
2019-05-03 23:29:40 -0500

    Update builddataset.py

--- builddataset.py
+++ builddataset.py
@@ -1,12 +1,12 @@
-#Building dataset
+#Building the dataset for project
 
-def buildTestSet(search_keyword):
+def build_rest_set_data(searchhashtag):
     try:
-        tweets_fetched = twitter_api.GetSearch(search_keyword, count = 100)
+        tweets_fetched = twitter_api.GetSearch(searchhashtag, count = 100)
         
-        print("Fetched " + str(len(tweets_fetched)) + " tweets for the term " + search_keyword)
+        print("Fetched " + str(len(tweets_fetched)) + " tweets for the term " + searchhashtag)
         
         return [{"text":status.text, "label":None} for status in tweets_fetched]
     except:
-        print("Unfortunately, something went wrong..")
+        print("Tweets not found")
         return None
