290ab7cf308e3a14af93fe6485c9f11c52528310
Apoorva Bapat <48076182+apoorva-bapat@users.noreply.github.com>
2019-05-03 23:29:08 -0500

    Update BuildTrainingDataSet.py

--- BuildTrainingDataSet.py
+++ BuildTrainingDataSet.py
@@ -1,34 +1,34 @@
-def buidTrainingSet(DataFile, tweetDataFile):
+def buidTraining_DataSet(DataFile, tweet_DataFile):
     import csv
     import time
     
-    corpus = []
+    data = []
     
     with open(DataFile,'rb') as csvfile:
-        lineReader = csv.reader(csvfile,delimiter=',', quotechar="\"")
-        for row in lineReader:
-            corpus.append({"tweet_id":row[2], "label":row[1], "topic":row[0]})
+        line_reader = csv.reader(csvfile,delimiter=',', quotechar="\"")
+        for row in line_reader:
+            data.append({"tweet_id":row[2], "label":row[1], "topic":row[0]})
             
-    rate_limit = 180
-    sleep_time = 900/180
+    rate_limit = 200
+    sleep_time = 900/200
     
-    trainingDataSet = []
+    training_data_set = []
     
-    for tweet in corpus:
+    for tweet in data:
         try:
             status = twitter_api.GetStatus(tweet["tweet_id"])
             print("Tweet fetched" + status.text)
             tweet["text"] = status.text
-            trainingDataSet.append(tweet)
+            training_data_set.append(tweet)
             time.sleep(sleep_time) 
         except: 
             continue
-    # now we write them to the empty CSV file
-    with open(tweetDataFile,'wb') as csvfile:
+    # writing to an empty csv file
+    with open(tweet_DataFile,'wb') as csvfile:
         linewriter = csv.writer(csvfile,delimiter=',',quotechar="\"")
-        for tweet in trainingDataSet:
+        for tweet in training_data_set:
             try:
                 linewriter.writerow([tweet["tweet_id"], tweet["text"], tweet["label"], tweet["topic"]])
             except Exception as e:
                 print(e)
-    return trainingDataSet
+    return training_data_set
