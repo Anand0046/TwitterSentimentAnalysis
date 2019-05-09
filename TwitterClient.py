8e02329fb6baa3f272875fc954360e675e65c297
Ananda Reddy Srinivasa Reddy <reddyanand604@gmail.com>
2019-05-09 11:31:41 -0500

    Delete TwitterClient.py

--- TwitterClient.py
+++
@@ -1,9 +0,0 @@
-class TwitterClient:
-    
-    def tw_connection():
-		#connecting to Twitter client        
-		return 1
-    def tw_authentication():
-		#Authenticate Twitter API connection
-		return 0
-        
